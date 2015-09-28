6bbcd92490c99cf15794fa686989bfe25571a8de
shoushoushou <shoushoushou@192.168.11.104>
2015-09-28 17:04:27 +0900

    aws db config

--- batch/config.sh
+++ batch/config.sh
@@ -2,12 +2,13 @@
 # 機能  :  config file
 # 作成  :  shoushou
 
-EB_ROOT=/Users/shoushoushou/git/ebusiness/batch
+#EB_ROOT=/Users/shoushoushou/git/ebusiness/batch
+EB_ROOT=/usr/local/src/ebusiness/batch
 
 #EB DB CONFIG
 EB_DB_HOST=localhost
-EB_DB_USER=quanpin_db
-EB_DB_PASS=quanpin_db
+EB_DB_USER=root
+EB_DB_PASS=eb2015
 EB_DB_PORT=3306
 EB_DB_NAME=eb
 
