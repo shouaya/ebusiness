a41934158265b46843edc142f17310f01b722d07
shoushoushou <shoushoushou@192.168.11.107>
2015-09-30 16:42:44 +0900

    bug fix

--- batch/backup.sh
+++ batch/backup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # 機能  :  backup eb datatables
 # 作成  :  shoushou
-source config.sh
+source /usr/local/src/ebusiness/batch/config.sh
 current_date=$(date "+%Y.%m.%d")
 mysqldump -u$EB_DB_USER -p$EB_DB_PASS -h$EB_DB_HOST $EB_DB_NAME > $EB_ROOT/backup/$current_date.sql --skip-add-locks
