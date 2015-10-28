c5b1f736988b9fedba2e79c1694ddc7eaccc8db5
YangWanjun <ywjsailor@gmail.com>
2015-10-28 17:28:28 +0900

    履歴書を画面で表示

--- eb/models.py
+++ eb/models.py
@@ -737,6 +737,66 @@ class HistoryProject(models.Model):
     def __unicode__(self):
         return "%s - %s %s" % (self.name, self.member.first_name, self.member.last_name)
 
+    def is_in_rd(self):
+        if self.stages.filter(name=u"要件定義").count() > 0:
+            return True
+        else:
+            return False
+
+    def is_in_sa(self):
+        if self.stages.filter(name=u"調査分析").count() > 0:
+            return True
+        else:
+            return False
+
+    def is_in_bd(self):
+        if self.stages.filter(name=u"基本設計").count() > 0:
+            return True
+        else:
+            return False
+
+    def is_in_dd(self):
+        if self.stages.filter(name=u"詳細設計").count() > 0:
+            return True
+        else:
+            return False
+
+    def is_in_pg(self):
+        if self.stages.filter(name=u"開発製造").count() > 0:
+            return True
+        else:
+            return False
+
+    def is_in_pt(self):
+        if self.stages.filter(name=u"単体試験").count() > 0:
+            return True
+        else:
+            return False
+
+    def is_in_it(self):
+        if self.stages.filter(name=u"結合試験").count() > 0:
+            return True
+        else:
+            return False
+
+    def is_in_st(self):
+        if self.stages.filter(name=u"総合試験").count() > 0:
+            return True
+        else:
+            return False
+
+    def is_in_maintain(self):
+        if self.stages.filter(name=u"保守運用").count() > 0:
+            return True
+        else:
+            return False
+
+    def is_in_support(self):
+        if self.stages.filter(name=u"サポート").count() > 0:
+            return True
+        else:
+            return False
+
 
 def create_group_salesperson():
     group_salesperson, created = Group.objects.get_or_create(name="Salesperson")
