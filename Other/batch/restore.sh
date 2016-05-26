02c9cc36c76fc0b2b1505affab283e05d0bfbf54
YangWanjun <ywjsailor@gmail.com>
2016-05-26 23:41:57 +1000

    Data transfor

--- Other/batch/restore.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# 機能  :  restore eb inside hr datatables
-# 作成  :  shoushou
-source /usr/local/src/ebusiness/batch/config.sh
-current_date=$(date "+%Y.%m.%d")
-mysql -u$EB_DB_USER -p$EB_DB_PASS $EB_DB_NAME < $EB_ROOT/download/$current_date.sql
