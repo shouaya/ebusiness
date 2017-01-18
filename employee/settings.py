7c0b6c94667b55e76057feb37396ac28a11c24d6
YangWanjun <ywjsailor@gmail.com>
2017-01-18 20:56:24 +0900

    DB接続先

--- employee/settings.py
+++ employee/settings.py
@@ -77,18 +77,18 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'eb_sales',
         'USER': 'root',
-        'PASSWORD': 'root',
-        'HOST': '',
-        'PORT': '',
+        'PASSWORD': 'eb2016',
+        'HOST': '54.65.139.168',
+        'PORT': '3306',
     },
 
     'bpm_eboa': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'bpm_eboa',
         'USER': 'root',
-        'PASSWORD': 'root',
-        'HOST': '',
-        'PORT': '',
+        'PASSWORD': 'eb2016',
+        'HOST': '54.65.139.168',
+        'PORT': '3306',
     },
 }
 
