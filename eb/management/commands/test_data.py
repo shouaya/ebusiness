58a702fb06930280a5212ebc469d614c7a16004c
YangWanjun <ywjsailor@gmail.com>
2017-02-02 15:19:35 +0900

    テストデータ導入バッチ

--- eb/management/commands/test_data.py
+++ eb/management/commands/test_data.py
@@ -22,7 +22,9 @@ class Command(BaseCommand):
         for table in tables:
             for row in table.Rows:
                 if not table.is_data_exist(row):
-                    table.insert_row(row)
+                    ret, sql = table.insert_row(row)
+                    if not ret:
+                        self.stdout.write('ERROR:' + sql)
         self.stdout.write(u"End...")
 
     def get_data_file(self):
@@ -50,11 +52,12 @@ class Command(BaseCommand):
             table = Table(table_name)
             tables.append(table)
             col_id_list = sheet.row_values(r + 2, start_colx=1)
+            col_type_list = sheet.row_values(r + 3, start_colx=1)
             # 項目ＩＤを取得
             col_count = 1
-            for col_id in col_id_list:
+            for col_id, col_type in zip(col_id_list, col_type_list):
                 if col_id:
-                    column = Column(col_id)
+                    column = Column(col_id, col_type)
                     table.Columns.append(column)
                     col_count += 1
             # 項目の値を取得する。
@@ -62,13 +65,19 @@ class Command(BaseCommand):
             while row_index < sheet.nrows and sheet.cell_value(row_index, 1) and sheet.cell_value(row_index, 1) != u"0件":
                 data_list = sheet.row_values(row_index, start_colx=1, end_colx=col_count)
                 row_data = []
-                for data in data_list:
+                for col_id, col_type, data in zip(col_id_list, col_type_list, data_list):
                     if data == 'NULL':
                         row_data.append(data)
                     elif data == 'False':
-                        row_data.append(0)
+                        row_data.append('0')
                     elif data == 'True':
-                        row_data.append(1)
+                        row_data.append('1')
+                    elif col_type in ('date'):
+                        row_data.append("date('" + data + "')")
+                    elif col_type in ('datetime'):
+                        row_data.append("datetime('" + data + "')")
+                    elif col_type in ('integer'):
+                        row_data.append(str(data))
                     else:
                         row_data.append("'" + data + "'")
                 table.Rows.append(row_data)
@@ -94,7 +103,13 @@ class Table:
 
     def to_insert_sql(self, row):
         col_id_list = [col.name for col in self.Columns]
-        sql = 'INSERT INTO %s (%s) VALUES (%s)' % (self.name, ', '.join(col_id_list), ', '.join(row))
+        try:
+            sql = 'INSERT INTO %s (%s) VALUES (%s)' % (self.name, ', '.join(col_id_list), ', '.join(row))
+        except:
+            print type(self.name), self.name
+            print type(col_id_list), col_id_list
+            print type(row), row
+            raise
 
         return sql
 
@@ -103,10 +118,12 @@ class Table:
         with connection.cursor() as cursor:
             try:
                 cursor.execute(sql)
+                return True, sql
             except:
-                print 'ERROR', sql
+                return False, sql
 
 
 class Column:
-    def __init__(self, name):
+    def __init__(self, name, data_type=None):
         self.name = name
+        self.data_type = data_type
