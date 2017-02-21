052b6a273e43b0f04bb279316951dd4d5e1ad4cd
YangWanjun <ywjsailor@gmail.com>
2017-02-21 21:26:05 +0900

    バッチ実行時間

--- employee/settings.py
+++ employee/settings.py
@@ -209,6 +209,6 @@ LOGGING = {
 
 CRONJOBS = [
     ('0 2 * * *', 'django.core.management.call_command', ['sync_members']),
-    ('0 9 * * *', 'django.core.management.call_command', ['member_status']),  # every day at 9:00
+    ('0 9 * * *', 'django.core.management.call_command', ['member_status']),
     ('0 6 25 * *', 'django.core.management.call_command', ['send_attendance_format']),
 ]
